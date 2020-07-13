618f6404ac400d31fd7aa477ad1a057e19094d09
Ilan Klinghofer <ilan.klinghofer@slalom.com>
2020-07-13 15:07:43 -0500

    Revert "chore: updated readme"
    This reverts commit 08ce90351dbd4427b48612f425df5787c68147aa.

--- ilan.js
+++
@@ -1,9 +0,0 @@
-const ngApiMock = require('./dist');
-
-ngApiMock.processor.process({
-  echo: true,
-  src: 'mocks',
-  watch: true
-});
-
-console.log(ngApiMock.processor.mocksProcessor.state._mocks[0].responses.okResponse.data);
